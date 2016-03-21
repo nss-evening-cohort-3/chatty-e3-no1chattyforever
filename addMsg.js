99195555cb3031beb35b90641ff15d7f56168a59
Abbie Holder <abigail.c.holder@gmail.com>
2016-03-21 18:58:03 -0500

    iife specs

---
+++ addMsg.js
@@ -0,0 +1,3 @@
+"use strict";
+
+//One IIFE should accept an element id, and the user message, and then add the user's message - along with the delete button - to the specified parent element. Each message should be stored in a private array in this IIFE. This IIFE should expose a function to read all messages, and delete a single message.
