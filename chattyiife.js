4103e3bcbc20eba4a36d59e1dba7278713ddc844
Sandy Jones <sandynashv@gmail.com>
2016-03-21 19:47:55 -0500

    removed Console.logs

--- chattyiife.js
+++ chattyiife.js
@@ -3,18 +3,14 @@ var Chatty = (function(chatty) {
   var chats = [];
   return {
     loadChats: function (callback) {
-    	console.log("loadChats");
       var chatLoader = new XMLHttpRequest();
       chatLoader.addEventListener("load", function () {
       var data = JSON.parse(this.responseText);
       chats = data.messages;
-      console.log("this works", chats);
       });
       chatLoader.open("GET", "chatty.json");
-      chatLoader.send();
-      
+      chatLoader.send(); 
     }
-
 	}
   return chatty;
 })(Chatty);
