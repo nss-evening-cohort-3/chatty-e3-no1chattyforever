41088e086c7feab812ac915d63a107dd2365d0fc
Sandy Jones <sandynashv@gmail.com>
2016-03-21 19:45:13 -0500

    Loader Iife

--- chattyiife.js
+++ chattyiife.js
@@ -1,18 +1,21 @@
 "use strict";
-var Chatty = (function(chatty(){
+var Chatty = (function(chatty) {
   var chats = [];
   return {
-    var loadChats = loadChats (function (callback) {
+    loadChats: function (callback) {
     	console.log("loadChats");
       var chatLoader = new XMLHttpRequest();
-      chatLoader.open("GET", "chatty.json");
-      chatLoader.send();
       chatLoader.addEventListener("load", function () {
-      loadChats = JSON.parse(this.responseText).inventory.cars;
-      callback(chats);	
+      var data = JSON.parse(this.responseText);
+      chats = data.messages;
       console.log("this works", chats);
       });
+      chatLoader.open("GET", "chatty.json");
+      chatLoader.send();
+      
     }
+
 	}
-	return chatty;
+  return chatty;
 })(Chatty);
+Chatty.loadChats();
