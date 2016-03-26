3bc94a1a0edec944b1d9577ba1735e2af1ee8743
Abbie Holder <abigail.c.holder@gmail.com>
2016-03-25 21:47:20 -0500

    delete button functional!

--- populate.js
+++ populate.js
@@ -7,12 +7,12 @@ var Chatty = (function(chatty) {
     let chatHistory = Chatty.getChats();
     // loop through array to output msgs in DOM
     for (let i = 0; i < chats.length; i++) {
-      // outputEl.innerHTML += '<p>' + chats[i].msg + '<button class="delete">' + "Delete" + '</button></p>';
+      let elementId = "chat"+i;
 
-      outputEl.innerHTML += '<span id="hover" class="deletebtn col-xs-1 glyphicon glyphicon-remove" aria-hidden="true"></span>' + '<p id="message" class="msg col-xs-11">' + chats[i].msg + '</p>';
+      outputEl.innerHTML += '<span id=' + elementId + ' class="deletebtn col-xs-1 glyphicon glyphicon-remove hover" aria-hidden="true"></span>' + '<p id="message" class="msg col-xs-11">' + chats[i].msg + '</p>';
       options();
     };
+    Chatty.setEventListener();
   }
   return chatty;
-
 }( Chatty || {} ));
